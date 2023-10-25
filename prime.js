0ee7324921856892d0a8dbb58d27007e927bd14d
Amzad Hussain <143213332+AmzadT@users.noreply.github.com>
2023-10-25 22:57:51 +0530

    Update prime.js

--- prime.js
+++ prime.js
@@ -1,15 +1,15 @@
 let num=7;
 let count=0
-for(i=1;i<=15;i++){
-  if(num%i!==0){
+for(let i=1;i<=num;i++){
+  if(num%i==0){
   count++
   }
 }
- if(count>2){
-  console.log("NotPrime")
+ if(count==2){
+  console.log("Prime")
 }
 else{
-  console.log("prime")
+  console.log("Not prime")
 }
 
 
