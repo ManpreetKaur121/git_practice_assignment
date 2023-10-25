6a429d7628551472c05fd245a0b40bec3b6a90b8
unknown <manpree.kaur.171819@gmail.com>
2023-10-25 19:59:59 +0530

    ff

---
+++ prime.js
@@ -0,0 +1,27 @@
+let num=7;
+let count=0
+for(i=1;i<=15;i++){
+  if(num%i!==0){
+  count++
+  }
+}
+ if(count>2){
+  console.log("NotPrime")
+}
+else{
+  console.log("prime")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
